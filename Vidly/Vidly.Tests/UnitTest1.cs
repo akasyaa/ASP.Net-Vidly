9d4e89ffaa9b90ce8a9799ab23261149044b5063
Sang Choi <akasyashc@gmail.com>
2016-12-12 01:41:26 -0800

    Stage 8 - Rental Form/API Controller

---
+++ Vidly/Vidly.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vidly.Controllers;
+
+namespace Vidly.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var customerController = new CustomersController();
+            var result = customerController.New() as ViewResult;
+            Assert.AreEqual("CustomerForm", result.ViewName);
+        }
+    }
+}
